7e43ccbd52fba5e6e670d3481f82828c1722d8ec
Wisien <wisnia3221@gmail.com>
2019-03-23 15:42:55 +0100

    Add massive unzip script

---
+++ massive-unzip.py
@@ -0,0 +1,37 @@
+from os import listdir
+from os.path import isfile, join
+import zipfile
+import os
+import sys
+
+
+def unzip_and_delate(file):
+    zip_ref = zipfile.ZipFile(file, 'r')
+    zip_ref.extractall(eTo)
+    zip_ref.close()
+    os.remove(file)
+
+
+leng = len(sys.argv)
+
+if leng > 2:
+        eFrom = sys.argv[1]
+        eTo = sys.argv[2]
+elif leng == 2:
+        eFrom = sys.argv[1]
+        eTo = sys.argv[1]
+else:
+        eFrom = "./"
+        eTo = "./"
+
+print("from:", eFrom)
+print("to:", eTo)
+
+files = []
+
+for f in listdir(eFrom):
+    filepath = join(eFrom, f)
+    if isfile(filepath) and filepath.endswith(".zip"):
+        print(f)
+        unzip_and_delate(f)
+        
